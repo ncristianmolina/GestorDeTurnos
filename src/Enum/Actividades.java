e966d20ba7a443e5644462163d692628847915bb
LuchiHamade <luciana.b.hamade@gmail.com>
2025-11-01 21:03:42 -0300

    Le agregué capacidad máxima a cada una de las actividades

--- src/Enum/Actividades.java
+++ src/Enum/Actividades.java
@@ -1,17 +1,27 @@
 package Enum;
 
 public enum Actividades {
-    FUNCIONAL,
-    MUSCULACION,
-    SPINNING,
-    BIKEFORCE,
-    ZUMBA,
-    MASAJE,
-    STRETCHING,
-    GAP,
-    NUTRICION,
-    YOGA,
-    PILATES,
-    RITMOSLATINOS,
-    ENTRENAMIENTOADULTOSMAYORES;
+    FUNCIONAL (25),
+    MUSCULACION(30),
+    SPINNING (15),
+    BIKEFORCE (15),
+    ZUMBA (30),
+    MASAJE (1),
+    STRETCHING (10),
+    GAP (10),
+    NUTRICION (1),
+    YOGA (20),
+    PILATES (10),
+    RITMOSLATINOS (20),
+    ENTRENAMIENTOADULTOSMAYORES (10);
+
+    private final int capacidadMax;
+
+    Actividades(int capacidadMax) {
+        this.capacidadMax = capacidadMax;
+    }
+
+    public int getCapacidadMax() {
+        return capacidadMax;
+    }
 }
