7d9737ce8a01afbfbd32c843e57ec2126c8a9d90
LuchiHamade <luciana.b.hamade@gmail.com>
2025-11-02 22:37:38 -0300

    Modifiqué atributo fecha y hora por fechaHora

--- src/Modelos/Turno.java
+++ src/Modelos/Turno.java
@@ -6,35 +6,43 @@ import Enum.EstadoTurno;
 
 public class Turno implements Reservable {
     private int idTurno;
-    private LocalDate fecha;
-    private LocalTime hora;
+    private LocalDate fechaHora;
     private EstadoTurno estado;
     private Cliente cliente;
     private Actividad Actividad;
 
-    public Turno(int idTurno, LocalDate fecha, LocalTime hora, EstadoTurno estado, Cliente cliente) {
+    public Turno(int idTurno, LocalDate fechaHora, EstadoTurno estado, Cliente cliente, Actividad actividad) {
         this.idTurno = idTurno;
-        this.fecha = fecha;
-        this.hora = hora;
+        this.fechaHora = fechaHora;
         this.estado = estado;
-
         this.cliente = cliente;
+        Actividad = actividad;
     }
 
     public int getIdTurno() { return idTurno; }
     public void setIdTurno(int idTurno) { this.idTurno = idTurno; }
 
-    public LocalDate getFecha() { return fecha; }
-    public void setFecha(LocalDate fecha) { this.fecha = fecha; }
+    public LocalDate getFechaHora() {
+        return fechaHora;
+    }
 
-    public LocalTime getHora() { return hora; }
-    public void setHora(LocalTime hora) { this.hora = hora; }
+    public void setFechaHora(LocalDate fechaHora) {
+        this.fechaHora = fechaHora;
+    }
 
     public EstadoTurno getEstado() { return estado; }
     public void setEstado(EstadoTurno estado) { this.estado = estado; }
 
     public Cliente getCliente() { return cliente; }
     public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    public Actividad getActividad() {
+        return Actividad;
+    }
+
+    public void setActividad(Actividad actividad) {
+        Actividad = actividad;
+    }
 
     @Override
     public void reservar() {
