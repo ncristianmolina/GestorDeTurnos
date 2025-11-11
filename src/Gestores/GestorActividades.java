718bc05237208155ec593bb991d024f6411e0aed
Julieta Requena <julirequena15@gmail.com>
2025-11-11 20:23:57 -0300

    GestorTurnos correccion método reservarTurno

--- src/Gestores/GestorActividades.java
+++ src/Gestores/GestorActividades.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestorActividades {
-    List<Actividad> actividades  = new ArrayList;
+    List<Actividad> actividades  = new ArrayList();
 
     public GestorActividades(List<Actividad> actividades) {
         this.actividades = actividades;
